fa6490ebb171cdafdefbd7f0b88ff9f15976910f
Kevin Matthews <kmatthews@cyberdata.net>
2017-11-02 18:14:40 -0700

    Set up cross-compilation for ATtiny861

---
+++ Toolchain-avr-ATtiny861.cmake
@@ -0,0 +1,13 @@
+# Load this Toolchain file for an out-of-source build using:
+#   cmake -DCMAKE_TOOLCHAIN_FILE=Toolchain-avr-ATtiny861.cmake ..
+set (AVR_MCU "attiny861")
+set (AVR_TOOLCHAIN_HOME /home/kmatthews/coding/toolchains/avr8-gnu-toolchain-linux_x86_64)
+
+set (CMAKE_SYSTEM_NAME Generic)
+set (CMAKE_C_COMPILER ${AVR_TOOLCHAIN_HOME}/bin/avr-gcc)
+set (CMAKE_CXX_COMPILER ${AVR_TOOLCHAIN_HOME}/bin/avr-g++)
+
+set (CMAKE_FIND_ROOT_PATH ${AVR_TOOLCHAIN_HOME})
+set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
